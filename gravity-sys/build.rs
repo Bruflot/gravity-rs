8eeb8bf62100f10f87ff29fd9c92259edb890a74
Bruflot <idar@bruflot.io>
2017-05-14 04:06:18 +0200

    Updated the build script & moved the raw bindings to a separate folder.

---
+++ gravity-sys/build.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::path::{Path, PathBuf};
+
+fn main(){
+    // This lets you specify the path of the Gravity library through the `GRAVITY_LIB_DIR`
+    // environment variable, *or* using a dynamic linker (e.g. `ld`).
+    let lib_path = env::var("GRAVITY_LIB_DIR");
+    match lib_path {
+        Ok(lib_path) => {
+            println!("cargo:rustc-link-search=native={}", lib_path);
+            println!("cargo:rustc-link-lib=dylib=gravity");
+        },
+        Err(e) => {
+            println!("Warning: `GRAVITY_LIB_DIR` not specified (E: {:?}) - defaulting to the \
+                      system's dynamic linker.", e);
+            println!("cargo:rustc-link-lib=dylib=gravity");
+        }
+    }
+}
